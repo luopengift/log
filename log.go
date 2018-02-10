24e33d5570fad28b97518a8afb5aed447ff5723b
luopengift <870148195@qq.com>
2018-02-10 19:24:42 +0800

    add mutli output endpoint

--- log.go
+++ log.go
@@ -22,12 +22,12 @@ type Log struct {
 	mux        *sync.Mutex
 	pool       sync.Pool //临时对象池
 	name       string
-	mode       int       //日志模式
-	level      uint8     //最低的告警级别
-	depth      int       //runtime.Call(depth)
-	delim      string    //每条日志的分隔符
-	timeFormat string    //日志时间格式
-	Formatter            //日志格式化接口,实现Format() string
+	mode       int         //日志模式
+	level      uint8       //最低的告警级别
+	depth      int         //runtime.Call(depth)
+	delim      string      //每条日志的分隔符
+	timeFormat string      //日志时间格式
+	Formatter              //日志格式化接口,实现Format() string
 	out        []io.Writer //日志输出点
 }
 
